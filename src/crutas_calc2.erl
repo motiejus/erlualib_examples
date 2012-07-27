265a57c4839f93f9c9b73d26db2f2c83b1ef8b48
Motiejus Jakštys <desired.mta@gmail.com>
2012-07-27 10:21:30 +0300

    More cleanup in crutas_calc2.erl

--- src/crutas_calc2.erl
+++ src/crutas_calc2.erl
@@ -1,7 +1,4 @@
 -module(crutas_calc2).
 -behaviour(gen_calculator).
-%-implemented_in("code:priv_dir(crutas) ++ \"/crutas_calc.lua\".").
-%-implemented_in("filename:join(filename:dirname(filename:dirname(code:which(?MODULE))), \"priv\") ++ \"/crutas_calc.lua\".").
 -implemented_in({priv, "/crutas_calc.lua"}).
-
 -compile({parse_transform, lua_behaviour}).
