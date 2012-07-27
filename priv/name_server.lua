e3c1326e1bb5e9b08a678bde30230f4d5b78269a
Motiejus Jakštys <desired.mta@gmail.com>
2012-07-27 13:57:28 +0300

    Prettify name_server.lua

--- priv/name_server.lua
+++ priv/name_server.lua
@@ -1,13 +1,15 @@
+-- Call function which name is req[1] (first element in incoming tuple)
+-- With tbl as the first argument, and other function arguments later.
+function call(tbl, req)
+    return _G[req[1]](tbl, unpack(req, 2))
+end
 
 function init()
     return erlang.atom("ok"), {} -- return empty state
 end
 
 function handle_call(req, from, tbl)
-    -- Call function which name is req[1] (first element in incoming tuple)
-    -- With tbl as the first argument, and other function arguments later.
-    ret, tbl = _G[req[1]](tbl, unpack(req, 2))
-    return erlang.atom("reply"), ret, tbl
+    return erlang.atom("reply"), call(tbl, req)
 end
 
 function add_name(tbl, name, address)
@@ -16,6 +18,5 @@ function add_name(tbl, name, address)
 end
 
 function get_addr(tbl, name)
-    io.stderr:write(name)
     return tbl[name] or erlang.atom("error"), tbl
 end
